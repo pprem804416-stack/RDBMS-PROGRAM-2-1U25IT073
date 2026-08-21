8d7a0d5ceb99d6e1892e8f320e9be9a145edcb46
pprem804416-stack <pprem804416@gmail.com>
2026-08-21 10:11:27 +0530

    RDBMS PROGRAM 2 1U25IT073

--- solution.sql
+++ solution.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE CollegeDB;
 USE CollegeDB;
-CREATE TABLE Student (
-    StudentID INT(5) PRIMARY KEY,
+CREATE TABLE Student(
+    StudentID INT(5) PRIMARY KEY, 
     StudentName VARCHAR(20),
-    DOB DATE,
+    DOB Date, 
     Gender VARCHAR(10),
     DepartmentID INT(5)
 );
-DESC Student;
+desc Student;
